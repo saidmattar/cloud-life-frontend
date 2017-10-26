171667320fbb61b2982fac073a95ce4eab913eb3
saidmattar <ssabdou@gmail.com>
2017-10-25 20:28:38 -0700

    little tweak to the navbar component to render the group form

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -24,6 +24,9 @@ class Navbar extends React.Component {
                 <li onClick={this.props.tokenDelete}><Link to="/">Logout</Link></li>
                 <li><Link to="/settings">Settings</Link></li>
                 <li><Link to="/dashboard">Dashboard</Link></li>
+
+                <li><Link to="/group">Grouppppppppp</Link></li>
+
               </div>
               :
               <div>
