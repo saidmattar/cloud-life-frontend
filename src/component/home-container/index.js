e7549a40847eadefcc7623e4c10b0ed03ffe8a08
mscharlock <michelle.scharlock@gmail.com>
2017-10-27 00:26:14 -0700

    Less ugly

--- src/component/home-container/index.js
+++ src/component/home-container/index.js
@@ -10,9 +10,9 @@ class HomeContainer extends React.Component {
       </div>
 
       <div className="center">
-        <p className="smallbox">Cloud Life is your life, in the cloud! We are a secure document storage solution for anyone who wants to keep their documents online.</p>
-        <p className="smallbox">Keep your documents organized in groups, which you can share with your friends!</p>
-        <p className="smallbox">Our storage uses professional tools like Amazon S3 storage to keep your documents safe</p>
+        <p className="smallbox" id="box1">Cloud Life is your life, in the cloud! We are a secure document storage solution for anyone who wants to keep their documents online.</p>
+        <p className="smallbox" id="box2">Keep your documents organized in groups, which you can share with your friends!</p>
+        <p className="smallbox" id="box3">Our storage uses professional tools like Amazon S3 storage to keep your documents safe</p>
       </div>
 
       </div>
